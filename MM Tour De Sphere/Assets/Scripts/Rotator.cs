e29a05bd616490f2967535775528918b1cd131ab
ec18240 <71559909+ec18240@users.noreply.github.com>
2020-10-18 16:53:13 +0100

    made some changes to objects
    changed the collectable objects and edited rotate script  (added few few assets from unity store)

--- MM Tour De Sphere/Assets/Scripts/Rotator.cs
+++ MM Tour De Sphere/Assets/Scripts/Rotator.cs
@@ -13,6 +13,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(-30, 45, 15) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 45, 15) * Time.deltaTime);
     }
 }
