0b7e4ab4fac076b3dbd97805735a9ea9647263c7
ec18240 <71559909+ec18240@users.noreply.github.com>
2020-10-22 16:05:11 +0100

    added mummy player
    added mummy player with scripts and free look camera

---
+++ MM Tour De Sphere/Assets/Scripts/Mummy_Player_Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController controller;
+
+    Animator MummyAnimator;
+
+    public float rotationSpeed =8;
+    public float movementSpeed =7;
+    public float gravity = 10;
+
+    Vector3 movementVector = Vector3.zero;
+
+    private float desiredAngleRoation = 0;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        MummyAnimator = GetComponent<Animator>();
+
+    }
+
+    public void MovementHandle(Vector2 input) {
+        if (controller.isGrounded) {
+            if (input.y > 0)
+            {
+                movementVector = transform.forward * movementSpeed;
+            }
+            else {
+                movementVector = Vector3.zero;
+                //MummyAnimator.SetFloat("move", 0);
+            }
+        }
+    }
+
+    public void HandleDirectionMovement(Vector3 direction) {
+        desiredAngleRoation = Vector3.Angle(transform.forward, direction);
+        var crossProduct = Vector3.Cross(transform.forward, direction).y;
+        if (crossProduct < 0)
+        {
+            desiredAngleRoation *= -1;
+        }
+
+    }
+
+    private void RotatePlayer() {
+        if (desiredAngleRoation > 10 || desiredAngleRoation < -10) {
+            transform.Rotate(Vector3.up * desiredAngleRoation * rotationSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void Update()
+    {
+        RotatePlayer();
+        movementVector.y -= gravity;
+        controller.Move(movementVector * Time.deltaTime);
+
+    }
+
+
+}
