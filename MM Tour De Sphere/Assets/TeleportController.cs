33d70c4c68124aea8c8a85de01bd8cdf169691bd
AshleyM1999QMUL <ec17486@qmul.ac.uk>
2020-11-26 18:59:48 +0000

    Teleport section added
    Teleport Section made to Mash's section

---
+++ MM Tour De Sphere/Assets/TeleportController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour
+{
+    public GameObject passenger; // Object being teleported
+    public Transform newPosition; //Coordinates of the new position
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialise();
+    }
+
+    void initialise() //Set inital values
+    {
+        if(passenger == null)
+        {
+            passenger = GameObject.FindWithTag("Player");
+        }
+        if(newPosition == null)
+        {
+            newPosition.position = new Vector3(0.0f, 0.0f, 0.0f); // Default position
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            UnityEngine.Debug.Log("HMMMMM");
+            passenger.transform.position = newPosition.position;
+        }
+    }
+}
